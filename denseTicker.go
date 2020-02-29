33f1cf7890d2ecdff52117de3cb5f092563bbc0a
hneemann <helmut.neemann@arcor.de>
2020-02-29 10:20:39 +0100

    adds a DenseTimeTicks

--- denseTicker.go
+++ denseTicker.go
@@ -4,9 +4,12 @@ import (
 	"gonum.org/v1/plot/vg"
 	"math"
 	"strconv"
+	"time"
 )
 
 var _ SizeTicker = &DenseTicks{}
+
+type stringSizer func(string) vg.Length
 
 // DenseTicks creates tick marks as dense as possible
 type DenseTicks struct {
@@ -17,17 +20,21 @@ type DenseTicks struct {
 	log       int
 
 	axisSize    vg.Length
-	stringSizer func(string) vg.Length
+	stringSizer stringSizer
 }
 
 func (mt *DenseTicks) SetAxis(axis Axis, axisLen vg.Length, orientation orientation) {
 	mt.axisSize = axisLen
+	mt.stringSizer = createStringer(axis, orientation)
+}
+
+func createStringer(axis Axis, orientation orientation) stringSizer {
 	if orientation == horizontal {
-		mt.stringSizer = func(s string) vg.Length {
+		return func(s string) vg.Length {
 			return axis.Tick.Label.Font.Width(s + "M")
 		}
 	} else {
-		mt.stringSizer = func(s string) vg.Length {
+		return func(s string) vg.Length {
 			return axis.Tick.Label.Font.Size * 2
 		}
 	}
@@ -129,4 +136,176 @@ func exp10(log int) float64 {
 		}
 	}
 	return exp10
+}
+
+var _ SizeTicker = &DenseTimeTicks{}
+
+// DenseTimeTicks creates tick marks as dense as possible
+type DenseTimeTicks struct {
+	// Format is used to format the date
+	Format string
+
+	// Time takes a float64 value and converts it into a time.Time.
+	// If nil, UTC unix time is used.
+	Time func(t float64) time.Time
+
+	// Float takes a time.Time value and converts it into a float64
+	// Must be the inverse of Time
+	Float func(t time.Time) float64
+
+	// Axis is required to transform the values from
+	// the data coordinate system to the graphic coordinate system
+	Axis Axis
+
+	axisSize    vg.Length
+	stringSizer stringSizer
+}
+
+func (t *DenseTimeTicks) SetAxis(axis Axis, axisLen vg.Length, orientation orientation) {
+	t.axisSize = axisLen
+	t.stringSizer = createStringer(axis, orientation)
+	t.Axis = axis
+}
+
+type dateModifier func(time time.Time) time.Time
+
+type incrementer struct {
+	incr, norm dateModifier
+}
+
+var incrementerList = []incrementer{
+	{daily(1), normTime},
+	{daily(2), normDay},
+	{weekly, normDay},
+	{twoWeekly, normDay},
+	{monthly(1), normDay},
+	{monthly(2), normMonth},
+	{monthly(3), normMonth},
+	{monthly(4), normMonth},
+	{monthly(6), normMonth},
+	{yearly(1), normMonth},
+	{yearly(2), normYear(2)},
+	{yearly(5), normYear(5)},
+	{yearly(10), normYear(10)},
+	{yearly(20), normYear(20)},
+}
+
+func normYear(i int) dateModifier {
+	return func(t time.Time) time.Time {
+		y := (t.Year() / i) * i
+		return time.Date(y, 1, 1, 0, 0, 0, 0, t.Location())
+	}
+}
+
+func normMonth(t time.Time) time.Time {
+	y := t.Year()
+	return time.Date(y, 1, 1, 0, 0, 0, 0, t.Location())
+}
+
+func normDay(t time.Time) time.Time {
+	y := t.Year()
+	m := t.Month()
+	return time.Date(y, m, 1, 0, 0, 0, 0, t.Location())
+}
+
+func normTime(t time.Time) time.Time {
+	y := t.Year()
+	m := t.Month()
+	d := t.Day()
+	return time.Date(y, m, d, 0, 0, 0, 0, t.Location())
+}
+
+func daily(days int) dateModifier {
+	return func(t time.Time) time.Time {
+		y := t.Year()
+		m := t.Month()
+		d := t.Day()
+		return time.Date(y, m, d+days, 0, 0, 0, 0, t.Location())
+	}
+}
+func weekly(t time.Time) time.Time {
+	y := t.Year()
+	m := t.Month()
+	d := t.Day() + 7
+	if d > 28 {
+		d = 1
+		m += 1
+	}
+	return time.Date(y, m, d, 0, 0, 0, 0, t.Location())
+}
+
+func twoWeekly(t time.Time) time.Time {
+	y := t.Year()
+	m := t.Month()
+	d := t.Day() + 14
+	if d > 28 {
+		d = 1
+		m += 1
+	}
+	return time.Date(y, m, d, 0, 0, 0, 0, t.Location())
+}
+
+func monthly(month time.Month) dateModifier {
+	return func(t time.Time) time.Time {
+		y := t.Year()
+		m := t.Month() + month
+		d := 1
+		return time.Date(y, m, d, 0, 0, 0, 0, t.Location())
+	}
+}
+
+func yearly(years int) func(t time.Time) time.Time {
+	return func(t time.Time) time.Time {
+		y := t.Year() + years
+		return time.Date(y, 1, 1, 0, 0, 0, 0, t.Location())
+	}
+}
+
+func (t *DenseTimeTicks) Ticks(min, max float64) []Tick {
+	if t.Time == nil || t.Float == nil {
+		t.Time = func(t float64) time.Time {
+			return time.Unix(int64(t), 0).In(time.UTC)
+		}
+		t.Float = func(t time.Time) float64 {
+			return float64(t.Unix())
+		}
+	}
+
+	minTime := t.Time(min)
+	size := t.stringSizer(minTime.Format(t.Format))
+
+	index := 0
+	for {
+		t0 := incrementerList[index].norm(minTime)
+		t1 := incrementerList[index].incr(t0)
+
+		space := vg.Length(t.Axis.Norm(t.Float(t1))-t.Axis.Norm(t.Float(t0))) * t.axisSize
+
+		if space > size || index == len(incrementerList)-1 {
+			break
+		}
+		index++
+	}
+
+	incrementer := incrementerList[index]
+	tickTime := incrementer.norm(minTime)
+
+	for t.Float(tickTime) < min {
+		tickTime = incrementer.incr(tickTime)
+	}
+
+	var ticker []Tick
+	for {
+		v := t.Float(tickTime)
+		if v > max {
+			break
+		}
+		ticker = append(ticker, Tick{
+			Value: v,
+			Label: t.Time(v).Format(t.Format),
+		})
+		tickTime = incrementer.incr(tickTime)
+	}
+
+	return ticker
 }
